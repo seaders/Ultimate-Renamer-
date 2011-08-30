836bc316f6e1e2e7f1f17f61c346cc28885a7162
seaders <seaders69@gmail.com>
2011-08-30 01:30:02 +0100

    Just about at rename point...

--- src/open_dir.py
+++ src/open_dir.py
@@ -1,7 +1,6 @@
 import sys, os, re
 from PyQt4 import QtGui
-from PyQt4 import QtCore
-from PyQt4.QtCore import SIGNAL as qsignal
+from PyQt4.QtCore import Qt, SIGNAL as qsignal
 from genericpath import isdir
 from epguides import find_or_add as getShow
 
@@ -13,6 +12,7 @@ class Example(QtGui.QMainWindow):
         self.initUI()
         
     def initUI(self):
+        self.setGeometry(100, 100, 1000, 300)
         self.container = QtGui.QWidget()
         self.setCentralWidget(self.container)
         
@@ -21,13 +21,13 @@ class Example(QtGui.QMainWindow):
         
         self.setWindowTitle('OpenFile')
         
-#        self.autoPilot()
+        self.autoPilot()
     
     def initMenuBar(self):
         openFile = QtGui.QAction(QtGui.QIcon('open.png'), '&Open', self)
         openFile.setShortcut('Ctrl+O')
         openFile.setStatusTip('Open new File')
-        self.connect(openFile, QtCore.SIGNAL('triggered()'), self.showDialog)
+        self.connect(openFile, qsignal('triggered()'), self.showDialog)
 
         fileMenu = self.menuBar().addMenu('&File')
         fileMenu.addAction(openFile)
@@ -37,28 +37,42 @@ class Example(QtGui.QMainWindow):
         self.before.setMaximumHeight(20)
         self.after = QtGui.QLabel()
         self.after.setMaximumHeight(20)
+        self.go = QtGui.QPushButton('Go!')
+        self.go.width = 40
         
         self.list = QtGui.QListWidget()
         self.connect(self.list, qsignal('itemClicked (QListWidgetItem *)'), self.listItemClicked)
  
         self.table = QtGui.QTableWidget()
-        self.table.setColumnCount(5)
-        self.table.setHorizontalHeaderLabels(['File Name', 'Parent Folder', 'Season', 'Episode', 'Title'])
+        self.initTableHeader()
         self.connect(self.table, qsignal('itemClicked (QTableWidgetItem *)'), self.tableItemClicked)
 
-        splitter = QtGui.QSplitter(QtCore.Qt.Horizontal)
+        splitter = QtGui.QSplitter(Qt.Horizontal)
         splitter.addWidget(self.list)
         splitter.addWidget(self.table)
-
-        hbox = QtGui.QHBoxLayout()
-        hbox.setSizeConstraint(QtGui.QHBoxLayout.SetNoConstraint)
-        hbox.addWidget(splitter)
-
+        splitter.setSizes([1, 200])
+        
+        mainbox = QtGui.QHBoxLayout()
+        mainbox.setSizeConstraint(QtGui.QHBoxLayout.SetNoConstraint)
+        mainbox.addWidget(splitter)
+        
+        tophbox = QtGui.QHBoxLayout()
+        tophbox.setSizeConstraint(QtGui.QHBoxLayout.SetNoConstraint)
+        
+        topvbox = QtGui.QVBoxLayout()
+        topvbox.addWidget(self.before)
+        topvbox.addWidget(self.after)
+        
+        topgbox = QtGui.QGroupBox()
+        topgbox.setLayout(topvbox)
+        
+        tophbox.addWidget(topgbox)
+        tophbox.addWidget(self.go)
+        
         vbox = QtGui.QVBoxLayout(self.container)
         vbox.setSizeConstraint(QtGui.QVBoxLayout.SetNoConstraint)
-        vbox.addWidget(self.before)
-        vbox.addWidget(self.after)
-        vbox.addLayout(hbox)
+        vbox.addLayout(tophbox)
+        vbox.addLayout(mainbox)
         
         
     def showDialog(self):
@@ -86,8 +100,18 @@ class Example(QtGui.QMainWindow):
         label.setText(text)
         label.adjustSize()
         
+    def getText(self, item):
+        return '' if item is None else str(item.text())
+    
+    def getTIText(self, row, col):
+        return self.getText(self.table.item(row, col))
+        
     def tableItemClicked(self, widget):
-        self.setLabelText(self.before, widget.text())
+        row = widget.row()
+        before = os.path.join(self.showLocation, self.getTIText(row, 2), self.getTIText(row, 1))
+        self.setLabelText(self.before, before)
+        after = os.path.join( self.showLocation, 'Season %d' % int(self.getTIText(row, 3)), '%02d - %s' % (int(self.getTIText(row, 4)), self.getTIText(row, 5)) )
+        self.setLabelText(self.after, after)
     
     CHECKS = [{'type': 'folder', 'pattern': '(?P<season>\d{1,2})'},
               {'type': 'filename', 'pattern': 'S(?P<season>\d{2})E(?P<episode>\d{2})'},
@@ -105,14 +129,17 @@ class Example(QtGui.QMainWindow):
         self.scanDir(root=self.showLocation, list=list)
         
         self.table.clearContents()
-        self.table.setRowCount(len(list))
-        row = 0
+        numRows = len(list)
+        self.table.setRowCount(numRows)
         
-        for file in list:
+        for row in range(numRows):
+            file = list[row]
             filename = file['filename']
             if not filename.endswith('avi') and not filename.endswith('mkv'):
                 continue
-            self.table.setItem(row, 0, self.newTWidgetItem(filename))
+            
+            self.table.setItem(row, 0, self.newCheckboxTWidgetItem())
+            self.table.setItem(row, 1, self.newTextTWidgetItem(filename))
             season = None
             episode = None
             folder = file['folder']
@@ -135,22 +162,21 @@ class Example(QtGui.QMainWindow):
                     both = season and episode
                     
             if not defaultFirstSeason:
-                self.table.setItem(row, 1, self.newTWidgetItem(folder))
+                self.table.setItem(row, 2, self.newTextTWidgetItem(folder))
             elif not season:
                 season = 1
             
             if season:
                 season = int(season)
-                self.table.setItem(row, 2, self.newTWidgetItem(season))
+                self.table.setItem(row, 3, self.newTextTWidgetItem(season))
             if episode:
                 episode = int(episode)
-                self.table.setItem(row, 3, self.newTWidgetItem(episode))
+                self.table.setItem(row, 4, self.newTextTWidgetItem(episode))
                 
             if season and episode and epguideShow:
                 if epguideShow.eps.get(season) and epguideShow.eps[season].get(episode):
-                    self.table.setItem(row, 4, self.newTWidgetItem(epguideShow.eps[season][episode]['name']))
-                
-            row += 1
+                    self.table.setItem(row, 5, self.newTextTWidgetItem(epguideShow.eps[season][episode]['name']))
+
             
     def checkMatch(self, matchObj, season, episode):
         both = False
@@ -161,9 +187,24 @@ class Example(QtGui.QMainWindow):
             both = True
         return season, episode, both
             
-    def newTWidgetItem(self, text):
+    def initTableHeader(self):
+        self.table.setColumnCount(6)
+        self.table.setHorizontalHeaderItem(0, self.newCheckboxTWidgetItem())
+
+        labels = ['File Name', 'Parent Folder', 'Season', 'Episode', 'Title']
+        for index in range(len(labels)):
+            self.table.setHorizontalHeaderItem(index+1, self.newTextTWidgetItem(labels[index]))
+            
+        self.table.horizontalHeader().setResizeMode(QtGui.QHeaderView.ResizeToContents)
+            
+    def newTextTWidgetItem(self, text):
         item = QtGui.QTableWidgetItem(QtGui.QTableWidgetItem.Type)
         item.setText(str(text))
+        return item
+    
+    def newCheckboxTWidgetItem(self):
+        item = QtGui.QTableWidgetItem()
+        item.setCheckState(Qt.Unchecked)
         return item
         
     def scanDir(self, list, root, folder=None):
